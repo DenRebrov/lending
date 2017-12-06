33341889fe69962585fcd83f1cc827f956a31f8c
Den Rebrov <den_rebrov@list.ru>
2017-12-06 13:33:02 +0300

    initial commit

---
+++ header.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.link').click(function(){
+    var id = $(this).attr('href');
+
+    $('html, body').animate({
+      scrollTop: ($(id).offset().top - 80)
+    }, 500);
+    return false;
+  });
+});
